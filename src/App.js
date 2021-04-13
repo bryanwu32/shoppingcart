ceca3237b616386e6da47856367ccd2845e70e07
bryanwu32 <bryanwu32@gmail.com>
2021-04-13 19:43:24 +0800

    git commit -m "First commit"

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="App-header">
+        <a href = "/">Hand-made market</a>
+      </header>
+      <main>
+        Products
+      </main>
+      <footer>
+        COMP3421 Web Application Design and Development
+      </footer>
+    </div>
+  );
+}
+
+export default App;
